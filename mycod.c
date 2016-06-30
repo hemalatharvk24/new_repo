7954183261b3d03806ff0353bd69b22fdf70b078
hemalatharvk24 <hemalatha.rvk24@gmail.com>
2016-06-30 11:17:44 +0530

    Create mycod.c

---
+++ mycod.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,index,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    scanf("%d",&index);
+    printf("%d",a[index]);
+    
+ 	return 
